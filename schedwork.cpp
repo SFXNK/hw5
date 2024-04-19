e284a4b3ba3d2230799655424c2107901023286e
Jason Zhao <zhanhaoz@usc.edu>
2024-04-19 05:13:36 +0000

    done

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool sdfs(int, const AvailabilityMatrix& , const size_t , std::vector<int> , DailySchedule& );
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,10 +36,42 @@ bool schedule(
         return false;
     }
     sched.clear();
+    sched.resize(avail.size());;
+    for(int i=0; i<sched.size(); i++){
+      sched[i].resize(dailyNeed);
+    }
     // Add your code below
-
-
-
-
+    std::vector<int> shiftsleft;
+    for(int i=0; i<avail[0].size(); i++){
+      shiftsleft.push_back(maxShifts);
+    }
+    return sdfs(0, avail, dailyNeed, shiftsleft, sched);
 }
 
+bool sdfs(
+    int task,
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    std::vector<int> shiftsleft,
+    DailySchedule& sched)
+{
+
+
+  if(task==avail.size()*dailyNeed){
+    return true;
+  }
+  //cout<<"day: "<<task/dailyNeed<<" task: "<<task%dailyNeed<<endl;
+  for(int j=0; j<avail[0].size(); j++){
+    if(avail[task/dailyNeed][j] && shiftsleft[j]>0 && (task%dailyNeed>0 ? sched[task/dailyNeed][task%dailyNeed-1]!=j : true) ){
+      shiftsleft[j]--;
+      sched[task/dailyNeed][task%dailyNeed]=j;
+      ///cout<<"day: "<<task/dailyNeed<<" task: "<<task%dailyNeed<<" worker: "<<j<<" left: "<<shiftsleft[j]<<endl;
+      if(sdfs(task+1, avail, dailyNeed, shiftsleft, sched))
+        return true;
+      shiftsleft[j]++;
+      sched[task/dailyNeed][task%dailyNeed]=0;
+    }
+  }
+  //cout<<"nothing"<<endl;
+  return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 // Add prototypes of helper functions here
 
+void wdfs(int, int , std::set<std::string>& , std::string , std::multiset<char> , const std::set<std::string>& );
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +22,57 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+  std::set<std::string> res;
+  std::multiset<char> fl;
+  for(int i=0; i<floating.length(); i++){
+    fl.insert(floating[i]);
+  }
+  int freen=0;
+  for(int i=0; i<in.length(); i++){
+    if(in[i]=='-')
+      freen++;
+  }
+  wdfs(freen, 0, res, in, fl, dict);
+  return res;
 }
 
 // Define any helper functions here
+void wdfs(int freen, int p, std::set<std::string>& res, std::string in, std::multiset<char> floating, const std::set<std::string>& dict){
+  if(p>=in.length()){
+    if(dict.find(in)!=dict.end() ){
+      //std::cout<<in<<endl;
+      res.insert(in);
+    }
+      
+    return;
+  }
+
+  if(in[p]!='-'){
+    wdfs(freen, p+1, res, in, floating, dict);
+    return;
+  }
+
+  if(floating.size()>0 && freen==floating.size()){
+    for(std::multiset<char>::iterator itr=floating.begin(); itr!=floating.end(); itr++){
+      std::multiset<char> fl=floating;
+      fl.erase(fl.find(*itr));
+      in[p]=*itr;
+      wdfs(freen-1, p+1, res, in, fl, dict);
+    }
+  }
+  else{
+    for(std::multiset<char>::iterator itr=floating.begin(); itr!=floating.end(); itr++){
+      std::multiset<char> fl=floating;
+      fl.erase(fl.find(*itr));
+      in[p]=*itr;
+      wdfs(freen-1, p+1, res, in, fl, dict);
+    }
+    for(char i='a'; i<='z'; i++){
+      if(floating.find(i)!=floating.end())
+        continue;
+      in[p]=i;
+      wdfs(freen-1, p+1, res, in, floating, dict);
+    }
+  }
+  
+}
